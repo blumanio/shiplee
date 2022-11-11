eea6e635792f37749ded3c1f3f6243636dbe16f9
DahmanCode <elaammariabderrahman@gmail.com>
2022-11-12 00:48:05 +0100

    make navbar responsive

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,20 +1,24 @@
-import React from "react"
+import React, { useState }from "react"
 import { Link } from "react-router-dom"
 import "./header.css"
 import shipping from "../../images/shipping.png"
 
 const Header = () => {
 
+  const [navlist, setnavlist] = useState(false)
+
   return (
     <>
       <header>
         <div className='container flex'>
           <div className="logo">
-            <img src={shipping} alt="" />
+            <a href="/">
+              <img src={shipping} alt="" />
+            </a>
           </div>
-          
+
           <div className="nav">
-            <ul className="flex">
+            <ul className={navlist ? "small" : "flex"}>
               <li>
                 <Link to='/'>Home</Link>
               </li>
@@ -27,11 +31,17 @@ const Header = () => {
             </ul>
           </div>
 
-          {/* <div className="toggle">
+          <div className="button flex">
             <button>
-              <i className="fa fa-bars"></i>
+              <i className="fa fa-sign-out"></i> Sign In
             </button>
-          </div> */}
+          </div>
+
+          <div className="toggle">
+            <button onClick={() => setnavlist(!navlist)}>
+              {navlist? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}
+            </button>
+          </div>
         </div>
       </header>
     </>
