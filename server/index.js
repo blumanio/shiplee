390df0dee63f4a607681e82ddf38c6ae43570549
Mohamed El Aammari <moelaammari@gmail.com>
2023-05-15 23:05:00 +0200

    deploy to heroku and fix backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
-
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -26,3 +28,19 @@ mongoose
   });
 // mongoose.set('useFindAndModify', false);
 //
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+// C:/cb/cb-js
+console.log(__dirname);
+
+// C:\cb\cb-js\index.html
+console.log(path.join(__dirname, "index.html"));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
