6e678b4bf15fa4801b659a633e322e7e64964bac
Mohamed El Aammari <moelaammari@gmail.com>
2023-05-16 22:47:36 +0200

    fix: refactor hard coded connection string to Heroku config var

--- server/index.js
+++ server/index.js
@@ -13,8 +13,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://medlique:medlique@cluster0.rzc9y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL)
