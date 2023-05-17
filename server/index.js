7c4926a214772f1b5a94b07002c9d3955da14b25
Mohamed El Aammari <moelaammari@gmail.com>
2023-05-17 15:15:18 +0200

    fix uri mongodb

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = process.env.MONGODB_URI;
+const CONNECTION_URL =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://medlique:medlique@cluster0.rzc9y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL)
