b1e3e9a3fc91c06864cf0aede6b6be448fd688cd
Mohamed El Aammari <moelaammari@gmail.com>
2023-05-18 12:19:24 +0200

    update endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const url = "http://localhost:5000/posts";
+const url = "https://backend-yjth.onrender.com/posts";
 export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) =>axios.post(url,newPost);
+export const createPost = (newPost) => axios.post(url, newPost);
